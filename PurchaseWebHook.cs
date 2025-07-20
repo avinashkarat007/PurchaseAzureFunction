ff6b597de858978739a8651bb2203da4a4797032
Avinash Karattuthodi <avinashkarattuthodi@outlook.com>
2025-07-19 22:19:54 -0700

    After trying for the third day, the output binding worked. Yaaaaaaaaaaaaaaaaaay!

--- PurchaseWebHook.cs
+++ PurchaseWebHook.cs
@@ -1,8 +1,11 @@
+using Azure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
 using static PurchaseWebHook.ProcessNewOrder;
 
 namespace PurchaseWebHook
@@ -25,16 +28,43 @@ namespace PurchaseWebHook
             _logger = logger;
         }
 
-        [Function(nameof(PurchaseWebHook))]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        [Function("PurchaseWebHook")]
+        public async Task<PurchaseWebHookResponse> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("Processing purchase webhook request...");
 
-            var order = req.ReadFromJsonAsync<NewOrderMessage>().Result;
+            var order = await JsonSerializer.DeserializeAsync<NewOrderMessage>(req.Body);
 
-            return new OkObjectResult($" {order.customerName} purchased {order.productName}!");
+            var response = req.CreateResponse();
+
+            if (order == null)
+            {
+                _logger.LogError("Invalid order data.");
+                response.StatusCode = HttpStatusCode.BadRequest;
+                await response.WriteStringAsync("Invalid order data.");
+                return new PurchaseWebHookResponse { HttpResponseData = response };
+            }
+
+            // Fix: Use the constructor of NewOrderMessage to initialize the object
+            var message = new NewOrderMessage(
+                productId: order.productId,
+                productName: order.productName,
+                quantity: order.quantity,
+                customerName: order.customerName,
+                price: order.price
+            );
+
+            response.StatusCode = HttpStatusCode.OK;
+
+            return new PurchaseWebHookResponse
+            {
+                Message = message,
+                HttpResponseData = response
+            };
         }
 
+        // This function is used to get the purchase information
         [Function(nameof(GetPurchase))]
         public IActionResult GetPurchase([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
@@ -45,7 +75,7 @@ namespace PurchaseWebHook
 
             var userAgent = req.Headers["User-Agent"].ToString();
 
-            return new OkObjectResult($"Hi { name }, Welcome to Azure Functions, using {userAgent}!");
+            return new OkObjectResult($"Hi {name}, Welcome to Azure Functions, using {userAgent}!");
         }
     }
 }
